ec47f3f2444897392a0cc00d8941a66e2b747897
MapleHe <qazwsx88tt@gmail.com>
2018-12-29 17:57:37 +0800

    (supplement) Backup the first version of some profiling related scripts.

---
+++ src/main/java/org/bgi/flexlab/metas/io/profilingio/ProfilingResultRecord.java
@@ -0,0 +1,52 @@
+package org.bgi.flexlab.metas.io.profilingio;
+
+import java.io.Serializable;
+
+/**
+ * ClassName: ProfilingResultRecord
+ * Description:
+ *
+ * @author: heshixu@genomics.cn
+ */
+
+public class ProfilingResultRecord implements Serializable {
+
+    protected String clusterName;
+    protected Integer rawReadCount;
+    protected Double abundance;
+    protected Double relativeAbun;
+    //protected Byte readNameString; //后续需要考虑采用更合适的方式来存储read name字符串
+
+    public void setClusterName(String name){
+        this.clusterName = name;
+    }
+
+    public String getClusterName(){
+        return this.clusterName;
+    }
+
+    public void setRawReadCount(Integer count){
+        this.rawReadCount = count;
+    }
+
+    public Integer getRawReadCount(){
+        return this.rawReadCount;
+    }
+
+    public void setAbundance(Double abundance) {
+        this.abundance = abundance;
+    }
+
+    public Double getAbundance(){
+        return this.abundance;
+    }
+
+    public void setRelativeAbun(Double relativeAbun){
+        this.relativeAbun = relativeAbun;
+    }
+
+    public Double getRelativeAbun(){
+        return this.relativeAbun;
+    }
+
+}
